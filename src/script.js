089d20e56d6e25b0e41a14d8914a3cb7f6ab8a6f
Simon <SimonDutkWork@gmail.com>
2022-01-21 23:32:49 +0100

    Dynamic adding days to the calendar

--- src/script.js
+++ src/script.js
@@ -13,9 +13,32 @@ const months = [
     'December',
 ];
 
+//Get current date
 let currentDate = new Date();
-
-//Dsiplay current month name
-const monthDisplay = document.getElementById('month');
+let currentYear = currentDate.getFullYear();
 let currentMonth = currentDate.getMonth();
+let currentDay = currentDate.getDay();
+
+//Display current month name
+const monthDisplay = document.getElementById('month');
 monthDisplay.innerHTML = months[currentMonth];
+
+//Get number of days in month
+const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+//Display days
+const days = document.getElementById('days');
+
+//Moves days to right place
+for (let i = 1; i <= currentDay; i++) {
+    let p = document.createElement('p');
+    p.innerHTML = '';
+    days.appendChild(p);
+}
+
+for (let i = 1; i <= daysInMonth; i++) {
+    let p = document.createElement('p');
+    p.innerHTML = i;
+
+    days.appendChild(p);
+}

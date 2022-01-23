bd510179f23e788a5b94dd572033eb7db60db5f6
Simon <SimonDutkWork@gmail.com>
2022-01-23 20:30:29 +0100

    Dynamic functions for moods palette & Adding moods for days

--- src/script.js
+++ src/script.js
@@ -32,13 +32,44 @@ const days = document.getElementById('days');
 //Moves days to right place
 for (let i = 1; i <= currentDay; i++) {
     let p = document.createElement('p');
-    p.innerHTML = '';
     days.appendChild(p);
 }
 
 for (let i = 1; i <= daysInMonth; i++) {
     let p = document.createElement('p');
+    p.classList.add('day-number');
+
     p.innerHTML = i;
 
     days.appendChild(p);
 }
+
+//Stores data about selected color
+
+// Get all colors
+let allColors = document.querySelectorAll('.moods-palette__mood__color');
+const colorsNames = ['colorPerfect', 'colorGood', 'colorSoSo', 'colorAwful'];
+
+let selectedColor = 0;
+const allDays = document.querySelectorAll('.day-number');
+
+allColors.forEach((element, index) =>
+    element.addEventListener('click', function () {
+        element.classList.remove('selected-color');
+        element.classList.add('selected-color');
+
+        allDays.forEach((element) =>
+            element.addEventListener('click', function () {
+                if (index === 0) {
+                    element.classList.add('day-number--perfect');
+                } else if (index === 1) {
+                    element.classList.add('day-number--good');
+                } else if (index === 2) {
+                    element.classList.add('day-number--so-so');
+                } else {
+                    element.classList.add('day-number--awful');
+                }
+            })
+        );
+    })
+);
